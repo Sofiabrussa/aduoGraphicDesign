7eb595e1b6a97160d2ae802dfa706479abc18802
Sofia Brussa <sofiabrussa@gmail.com>
2024-12-11 07:57:10 -0300

    sectionCards

---
+++ aduographic/src/components/SectionCards/SectionCards.jsx
@@ -0,0 +1,31 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./SectionCardsStyles.css";
+
+
+function SectionNosotras() {
+
+    return (
+
+        <div className='sectionCards'>
+            <div class="card cardChichas">
+                <img src="/imgCards/0ba1b57d50a98b9287eebd68b93882b0.jpg" class="card-img-top" alt="..."/>
+                    {/* <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            
+                    </div> */}
+            </div>
+            <div class="cardChichas">
+                <img src="/imgCards/8952e74f5cef2b0ee312d671b86e6b12.jpg" class="card-img-top" alt="..."/>
+                    {/* <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            
+                    </div> */}
+            </div>
+        </div>
+    )
+}
+
+export default SectionNosotras;

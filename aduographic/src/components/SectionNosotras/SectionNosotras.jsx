a2e3d517efb0ce92c45d6a5bc38006e0fa594ea8
Sofia Brussa <sofiabrussa@gmail.com>
2024-12-12 09:43:30 -0300

    sectionNosotras

--- aduographic/src/components/SectionNosotras/SectionNosotras.jsx
+++ aduographic/src/components/SectionNosotras/SectionNosotras.jsx
@@ -15,29 +15,30 @@ function SectionNosotras() {
     return (
         <div className="container-fluid vh-100 d-flex flex-column m-0 p-0">
             <div className="container-fluid d-flex flex-row m-0 p-0 flex-grow-1" style={{ height: "70%" }}>
-                <div className="sectionNosotrasText container-fluid m-0 p-0 d-flex flex-column justify-content-center align-items-center">
-                    <h1 className="m-3 p-0 w-50 fs-2">Somos un estudio gráfico que nació para <span> romper esquemas. </span></h1>
-                    <p className="m-3 p-0 w-50 fs-5 ">En A Dúo, somos dos amigas apasionadas por transformar ideas en experiencias visuales únicas.</p>
-                    <p className="m-3 p-0 w-50 fs-5">Nos mueve la creatividad, la conexión con nuestros clientes y el desafío de crear marcas que sean tan auténticas como vos.</p>
+                <div className="sectionNosotrasText container-fluid m-0 p-0 d-flex flex-column justify-content-center align-items-center" >
+                    <h1 className="m-3 p-0 w-50" style={{fontSize: "clamp(1rem, 4vw, 2rem)"}}> Somos un estudio gráfico que nació para <span> romper esquemas. </span></h1>
+                    <p className="m-3 p-0 w-50 fs-5 fs-md-6 fs-sm-7 ">En A Dúo, somos dos amigas apasionadas por transformar ideas en experiencias visuales únicas.</p>
+                    <p className="m-3 p-0 w-50 fs-5 fs-md-6 fs-sm-7">Nos mueve la creatividad, la conexión con nuestros clientes y el desafío de crear marcas que sean tan auténticas como vos.</p>
                     <div className="container-fluid m-5 p-0 w-50 d-flex align-items-center">
                         <Button>Trabajemos juntos </Button>
                     </div>
                 </div>
-                <div className="container-fluid m-0 p-0" style={{ height: "100%" }}>
+                <div className="container-fluid m-0 p-0 d-flex justify-content-center" style={{ height: "100%" }}>
                     <img
                         src="/imgsNosotras/imagennosotras.png"
                         alt="Imagen de fondo"
-                        className="w-100"
-                        style={{ objectFit: "contain", height: "100%", maxHeight: "70%" }}
+                        className="img-fluid"
+                        style={{ objectFit: "contain", height: "100%" }}
                     />
                 </div>
             </div>
-            <div className="container-fluid bg-secondary text-white m-0 p-0" style={{ height: "30%" }}>
-                <div className="sectionCon-Carrousel-nosotras h-100 w-100">
-                    <Carousel images={imagesCarouselSectionNosotras} className="w-100 h-100"></Carousel>
+            <div className="container-fluid m-0 p-0" style={{ height: "30%" }}>
+                <div className=" h-100 w-100" style={{ overflow: "hidden" }}>
+                    <Carousel images={imagesCarouselSectionNosotras} className="h-100 w-100"/>
                 </div>
             </div>
         </div>
+
     );
 }
 

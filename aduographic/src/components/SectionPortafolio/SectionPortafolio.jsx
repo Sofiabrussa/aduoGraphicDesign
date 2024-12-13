07bea94628e591df5f5cd85f44b6a523231f6475
Sofia Brussa <sofiabrussa@gmail.com>
2024-12-13 07:35:18 -0300

    responsiveServiucios

--- aduographic/src/components/SectionPortafolio/SectionPortafolio.jsx
+++ aduographic/src/components/SectionPortafolio/SectionPortafolio.jsx
@@ -4,10 +4,10 @@ import "./SectionPortafolioStyles.css"
 
 function SectionPortafolio() {
     return (
-        <div className="container-fluid vh-100 d-flex flex-column m-0 p-0" style={{ maxHeight: '100vh' }}>
+        <div className="container-fluid vh-100 d-flex flex-column m-0 p-0" style={{ maxHeight: '100vh', overflow: "hidden" }}>
             {/* Componente texto */}
             <div className="d-flex justify-content-center align-items-center sectionPortfolioText flex-shrink-1 flex-grow-0" style={{ minHeight: "150px" }}>
-                <h1 style={{ fontSize: "clamp(1rem, 4vw, 2rem)" }}>
+                <h1>
                     <span style={{ color: "var(--second-violet)" }}>branding </span> y {" "}
                     <span style={{ color: "var(--second-violet)" }} >diseño</span> pensados para que <br />
                     <span style={{ fontWeight: 400 }}>tu esencia explote</span> en cada detalle.
@@ -16,7 +16,7 @@ function SectionPortafolio() {
 
             {/* Componente img y lista */}
             <div className="d-flex justify-content-center align-items-center flex-row" style={{ overflow: "hidden" }}>
-                <div className='w-50' style={{ height: "100%" }}>
+                <div className='w-50 mx-auto d-block' style={{maxWidth:"1000px", minWidth:"250px" }}>
                     <img
                         src="/imgPortfolio/imagenservicios.png"
                         alt="Imagen de fondo"
@@ -49,7 +49,7 @@ function SectionPortafolio() {
             {/* Componente boton */}
             <div className="flex-shrink-1 flex-grow-0 d-flex justify-content-center align-items-center" style={{ minHeight: "200px", maxHeight: "250px" }}>
             <a href="https://www.behance.net/aduographicdesign" target="_blank" rel="noopener noreferrer">
-                <button class="btn btn-outline-dark  buttonSectionPortfolio rounded-4 " style={{ fontSize: "clamp(1rem, 4vw, 1.5rem)", padding: "0.5rem 1.5rem" }}>
+                <button class="btn btn-outline-dark  buttonSectionPortfolio rounded-4 " style={{ fontSize: "1.5rem", padding: "0.5rem 1.5rem" }}>
                     Portfolio↗
                 </button>
                 </a>

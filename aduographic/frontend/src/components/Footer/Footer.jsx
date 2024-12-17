8f7f09183ca26a2564894a55c5bea149c21ba054
Sofia Brussa <sofiabrussa@gmail.com>
2024-12-17 20:21:48 -0300

    footerListo

--- aduographic/frontend/src/components/Footer/Footer.jsx
+++ aduographic/frontend/src/components/Footer/Footer.jsx
@@ -5,18 +5,18 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 function Footer() {
     return (
         <div id='Footer' className="container-fluid w-100 bg-dark d-flex flex-column">
-            <div className="row g-1 d-flex align-items-center">
+            <div className="row g-1 d-flex align-items-center mb-4 mt-2 ">
                 <div className="col">
                     <img src="/imgsInicio/recursos-03.png" alt="Logo de mi empresa" className="navbar-logo" />
                 </div>
                 <div className="col">
-                    <i className="fab fa-instagram text-white" style={{ fontSize: '2.5rem' }}></i>
+                    <i className="fab fa-instagram text-white" style={{ fontSize: '3rem' }}></i>
                 </div>
                 <div className="col">
-                    <i className="fab fa-behance" style={{ fontSize: '2.5rem', color: 'white' }}></i>
+                    <i className="fab fa-behance" style={{ fontSize: '3rem', color: 'white' }}></i>
                 </div>
                 <div className="col">
-                    <i className="fab fa-whatsapp" style={{ fontSize: '2.5rem', color: 'white' }}></i>
+                    <i className="fab fa-whatsapp" style={{ fontSize: '3rem', color: 'white' }}></i>
                 </div>
                 <div className="col">
                     <ul className="list-unstyled text-white">

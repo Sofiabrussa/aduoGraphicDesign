325785c3ccae398752381ea2f54d859d245462a1
Sofia Brussa <sofiabrussa@gmail.com>
2024-12-18 08:16:56 -0300

    arreglosVarios

--- aduographic/frontend/src/components/Inicio/Inicio.jsx
+++ aduographic/frontend/src/components/Inicio/Inicio.jsx
@@ -11,22 +11,22 @@ function Inicio() {
     const imagesCarouselInicio = [
         { src: "/imgsInicio/degrade1.png", alt: "IMAGEN1" },
         { src: "/imgsInicio/degrade2.png", alt: "IMAGEN2" },
-      ];
+    ];
 
     return (
         <div id="carouselExample" className="carousel slide" >
             <div className="carousel-fixed-text">
-                <h1 className="carousel-h1">NO <span style={{ color: 'rgb(206, 181, 216)'}}> hay </span> DOS <br /> SIN <span style={{ color: 'rgb(206, 181, 216)' }}>tres</span></h1>
+                <h1 className="carousel-h1">NO <span style={{ color: 'rgb(206, 181, 216)' }}> hay </span> DOS <br /> SIN <span style={{ color: 'rgb(206, 181, 216)' }}>tres</span></h1>
                 <p>Hacemos que tu marca <br /> sea lo que siempre soñaste </p>
-                <button type="button" class="btn btn-outline-primary buttom-aduo">
-                    Contactanos ↗
-                </button>
+                <a href="#contacto" class="btn btn-outline-primary buttom-aduo">
+                    Contáctanos ↗
+                </a>
             </div>
 
             <Carousel images={imagesCarouselInicio} className="h-100 w-100" > </Carousel>
 
             <div className="carousel-iconos">
-                <div className="icono"> 
+                <div className="icono">
                     <a href="https://walink.co/9f29a0" target="_blank" rel="noopener noreferrer">
                         <img src="/imgsInicio/recursos_Mesadetrabajo 1.png" alt="..." />
                     </a>
--- aduographic/frontend/src/components/SectionNosotras/SectionNosotras.jsx
+++ aduographic/frontend/src/components/SectionNosotras/SectionNosotras.jsx
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./SectionNosotrasStyles.css";
-import Button from '../button/button';
 import Carousel from '../Carrousel/Carrousel';
 
 function SectionNosotras() {
@@ -13,14 +12,14 @@ function SectionNosotras() {
         ];
         
     return (
-        <div id='nosotras' className="container-fluid vh-100 d-flex flex-column m-0 p-0" style= {{maxHeight: "100vh"}} >
+        <div id='nosotras' className="container-fluid d-flex flex-column m-0 p-0" style= {{maxHeight: "100vh"}} >
             <div className="container-fluid d-flex flex-row m-0 p-0 flex-grow-1" style={{ height: "75%" }}>
                 <div className="sectionNosotrasText container-fluid m-0 p-0 d-flex flex-column justify-content-center align-items-center" >
                     <h1 className="m-3 p-0 w-50"> Somos un estudio gráfico que nació para <span> romper esquemas. </span></h1>
                     <p className="m-3 p-0 w-50  ">En A Dúo, somos dos amigas apasionadas por transformar ideas en experiencias visuales únicas.</p>
                     <p className="m-3 p-0 w-50 ">Nos mueve la creatividad, la conexión con nuestros clientes y el desafío de crear marcas que sean tan auténticas como vos.</p>
                     <div className="container-fluid m-5 p-0 w-50 d-flex align-items-center">
-                        <Button>Trabajemos juntos </Button>
+                        <a href='https://walink.co/9f29a0' className="buttom-aduo button-Nosotras">Trabajemos juntos </a>
                     </div>
                 </div>
                 <div className="container-fluid m-0 p-0 d-flex justify-content-center img-container">

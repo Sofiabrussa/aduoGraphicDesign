106a387a24d4199bd22269304235a1dde1ffb1d6
Sofia Brussa <sofiabrussa@gmail.com>
2025-02-03 09:31:22 -0300

    ..

--- aduographic/frontend/src/components/SectionCards/SectionCards.jsx
+++ aduographic/frontend/src/components/SectionCards/SectionCards.jsx
@@ -2,38 +2,43 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./SectionCardsStyles.css";
 
-
 function SectionCards() {
     return (
-        <div id='SectionCards' className="container-fluid d-flex justify-content-center align-items-center text-center">
-            <div className="card rounded-5 position-relative overflow-hidden">
-                <img
-                    src="/imgCards/valu.png"
-                    className="card-img-top w-100 h-auto"
-                    alt="Imagen 1"
-                    style={{ objectFit: "cover" }}
-                />
-                <div className="overlay d-flex justify-content-end align-items-center flex-column p-5">
-                    <h5 className="card-title"><b>Valentina Tajchman</b></h5>
-                    <p className="card-text">Diseñadora gráfica</p>
+        <div id='SectionCards' className="container-fluid ">
+            <div className="row justify-content-center align-items-center min-vh-100">
+                <div className="col-12 col-md-5 col-lg-3 mb-4">
+                    <div className="card card-animate rounded-5 position-relative overflow-hidden">
+                        <img
+                            src="/imgCards/valu.png"
+                            className="card-img-top w-100"
+                            alt="Valentina Tajchman"
+                            loading="lazy"
+                        />
+                        <div className="overlay d-flex justify-content-end align-items-center flex-column p-4">
+                            <h5 className="card-title mb-2"><b>Valentina Tajchman</b></h5>
+                            <p className="card-text">Diseñadora gráfica</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className="card rounded-5 position-relative overflow-hidden">
-                <img
-                    src="/imgCards/jose.png"
-                    className="card-img-top w-100 h-auto"
-                    alt="Imagen 2"
-                    style={{ objectFit: "cover" }}
-                />
-                <div className="overlay d-flex justify-content-end align-items-center flex-column p-5">
-                    <h5 className="card-title"><b>María Josefina Bas</b></h5>
-                    <p className="card-text">Diseñadora gráfica</p>
+                <div className="col-12 col-md-5 col-lg-3 mb-4">
+                    <div className="card card-animate rounded-5 position-relative overflow-hidden">
+                        <img
+                            src="/imgCards/jose.png"
+                            className="card-img-top w-100"
+                            alt="María Josefina Bas"
+                            loading="lazy"
+                        />
+                        <div className="overlay d-flex justify-content-end align-items-center flex-column p-4">
+                            <h5 className="card-title mb-2"><b>María Josefina Bas</b></h5>
+                            <p className="card-text">Diseñadora gráfica</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
-
 export default SectionCards;
+
 
 

6c0475d56f570b942773caa5041353dd703694a0
Sofia Brussa <sofiabrussa@gmail.com>
2025-02-03 10:33:26 -0300

    arreglitos god

--- aduographic/frontend/src/components/Servicios/Desarrollo/Desarrollo.jsx
+++ aduographic/frontend/src/components/Servicios/Desarrollo/Desarrollo.jsx
@@ -1,19 +1,22 @@
 import "./DesarrolloStyled.css";
-
-
 function Desarrollo() {
     return (
-        <div id="section-desarrollo" className="d-flex text-center flex-column align-items-center justify-content-center">
-            <h1> Diseño y <br/> <span> desarrollo web </span> </h1>
-            <p>  Diseñamos y desarrollamos tu página web a
-                tu medida. Ya sea que estés buscando una
-                landing page, una web sencilla o un e-commerce.
-                Diseñar webs se nos da reee cool!</p>
-            <button className="btn-custom "> lo necesito </button>
-
-
+        <div id="section-desarrollo" className="container-fluid d-flex text-center flex-column align-items-center justify-content-center py-4 py-md-5">
+            <div className="row w-100">
+                <div className="col-12">
+                    <h1 className="mb-3 mb-md-4"> 
+                        Diseño y <br className="d-md-none"/> <span> desarrollo web </span> 
+                    </h1>
+                    <p className="mx-auto">  
+                        Diseñamos y desarrollamos tu página web a
+                        tu medida. Ya sea que estés buscando una
+                        landing page, una web sencilla o un e-commerce.
+                        Diseñar webs se nos da reee cool!
+                    </p>
+                    <button className="btn-custom"> lo necesito </button>
+                </div>
+            </div>
         </div>
     )
 }
-
 export default Desarrollo;

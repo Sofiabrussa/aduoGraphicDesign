b372869f1f61064b883307aa1309e4c4cd79b651
Sofia Brussa <sofiabrussa@gmail.com>
2025-05-02 19:17:59 -0300

    tamaños texto mejorado

--- aduographic/frontend/src/components/SectionPortafolio/PortfolioTitle.jsx
+++ aduographic/frontend/src/components/SectionPortafolio/PortfolioTitle.jsx
@@ -1,20 +1,24 @@
 import { Card } from 'react-bootstrap'
+import "./SectionPortafolioStyles.css"
 
 const PortfolioTitle = () => {
-    return (
-      <Card className="my-3 d-flex justify-content-center align-items-center sectionPortfolioText" style={{ border: "1px solid white" }}>
-        <Card.Title className="text-center">
-          <span className="SpanPortfolioHand" style={{ color: "var(--second-violet)" }}>
-            branding{" "}
-          </span>{" "}
-          y{" "}
-          <span className="SpanPortfolioHand" style={{ color: "var(--second-violet)" }}>
-            diseño
-          </span>{" "}
+  return (
+    <Card className="my-3 d-flex justify-content-center align-items-center sectionPortfolioText" style={{ border: "1px solid white" }}>
+      <Card.Title className="text-center">
+        <span className="SpanPortfolioHand" style={{ color: "var(--second-violet)" }}>
+          branding
+        </span>{" "}
+        <span className="OtherText">y</span>{" "}
+        <span className="SpanPortfolioHand" style={{ color: "var(--second-violet)" }}>
+          diseño
+        </span>{" "}
+        <span className="OtherText">
           pensados para que <br />
-          <span style={{ fontWeight: 500 }} >tu esencia explote </span> en cada detalle.
-        </Card.Title>
-      </Card>
-    );
-  };
-  export default PortfolioTitle;
+          <span style={{ fontWeight: 500 }}>tu esencia explote</span> en cada detalle.
+        </span>
+      </Card.Title>
+    </Card>
+  );
+};
+
+export default PortfolioTitle;

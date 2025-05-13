1506521e78b40c49781acbf5bf2b6c672263ed11
Sofia Brussa <sofiabrussa@gmail.com>
2025-05-13 07:46:54 -0300

    cambios cards listos

--- aduographic/frontend/src/components/SectionCards/CustomCard.jsx
+++ aduographic/frontend/src/components/SectionCards/CustomCard.jsx
@@ -38,8 +38,8 @@ function CustomCard({ image, title, text }) {
                             animate={{ y: hovered ? 0 : 20 }}
                             transition={{ duration: 0.3 }}
                         >
-                            <Card.Title className="text-center">{title}</Card.Title>
-                            <Card.Text className="text-center">{text}</Card.Text>
+                            <Card.Title className="text-center mb-0">{title}</Card.Title>
+                            <Card.Text className="text-center  mt-0">{text}</Card.Text>
                         </motion.div>
                     </Card.ImgOverlay>
                 </Card>

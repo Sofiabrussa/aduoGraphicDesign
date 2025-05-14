a5be61d32d1fc982e04435c61309dcd805d5f465
Sofia Brussa <sofiabrussa@gmail.com>
2025-05-14 10:21:38 -0300

    ..

--- aduographic/frontend/src/components/Servicios/Desarrollo/Desarrollo.jsx
+++ aduographic/frontend/src/components/Servicios/Desarrollo/Desarrollo.jsx
@@ -17,6 +17,7 @@ function Desarrollo() {
                     <Card.Text className="mb-4">
                         Diseñamos y desarrollamos tu página web a tu medida. <br/>
                         Ya sea que estés buscando una landing page, una web sencilla o un e-commerce.
+                        <br/>
                         <strong>¡Diseñar webs se nos da reee cool!</strong>
                     </Card.Text>
                     <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
